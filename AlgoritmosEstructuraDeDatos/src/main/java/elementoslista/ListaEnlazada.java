71e86b81067674d68ed9b05225e503346542832c
Rudyg <Rudyg@VictusRudy>
2025-03-01 21:22:16 -0600

    Implementacion de Log4j

--- AlgoritmosEstructuraDeDatos/src/main/java/elementoslista/ListaEnlazada.java
+++ AlgoritmosEstructuraDeDatos/src/main/java/elementoslista/ListaEnlazada.java
@@ -1,6 +1,10 @@
 package elementoslista;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ListaEnlazada {
+    private static final Logger logger = LogManager.getLogger(ListaEnlazada.class);
     private Nodo cabeza;
 
     public ListaEnlazada() {
@@ -15,23 +19,28 @@ public class ListaEnlazada {
         Nodo nuevoNodo = new Nodo(dato);
         nuevoNodo.siguiente = cabeza;
         cabeza = nuevoNodo;
+        logger.info("Se agregó un nuevo nodo con el dato: " + dato);
     }
 
     public void eliminar() {
         if (estaVacia()) {
-            System.out.println("La lista está vacía, no se puede eliminar.");
+            logger.warn("La lista está vacía, no se puede eliminar.");
             return;
         }
+        logger.info("Se eliminó el nodo con el dato: " + cabeza.dato);
         cabeza = cabeza.siguiente;
     }
 
     public void imprimir() {
+        StringBuilder sb = new StringBuilder();
         Nodo actual = cabeza;
         while (actual != null) {
-            System.out.print(actual.dato + " -> ");
+            sb.append(actual.dato).append(" -> ");
             actual = actual.siguiente;
         }
-        System.out.println("null");
+        sb.append("null");
+        logger.info("Lista actual: " + sb.toString());
     }
 }
+
 
--- AlgoritmosEstructuraDeDatos/src/main/java/main/Main.java
+++ AlgoritmosEstructuraDeDatos/src/main/java/main/Main.java
@@ -1,29 +1,28 @@
 package main;
 
 import elementoslista.ListaEnlazada;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         ListaEnlazada lista = new ListaEnlazada();
 
-       
-        System.out.println("La lista está vacia? " + lista.estaVacia());
+        logger.info("¿La lista está vacía? " + lista.estaVacia());
 
-     
         lista.agregar(10);
         lista.agregar(20);
         lista.agregar(30);
 
-   
-        System.out.println("Lista con los elementos agregados:");
+        logger.info("Lista con los elementos agregados:");
         lista.imprimir();
 
-   
         lista.eliminar();
-        System.out.println("Lista con un elemento eliminado:");
+        logger.info("Lista con un elemento eliminado:");
         lista.imprimir();
 
-        
-        System.out.println("La lista está vacia? " + lista.estaVacia());
+        logger.info("¿La lista está vacía? " + lista.estaVacia());
     }
 }
